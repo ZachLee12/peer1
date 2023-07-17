0b82e16a324394dee769b1655d8edb4bea4b02f5
ZachLee12 <leezhengyang22@gmail.com>
2023-07-17 10:47:34 +0200

    Bidirectional communication from Philipp to Zach

--- public/peer1/peer1.js
+++ public/peer1/peer1.js
@@ -4,29 +4,43 @@ const peer1 = new Peer("Philipp", {
     port: 9000,
     path: "/",
 })
+const sendMsgBtn = document.querySelector('.btn')
 
+//Peer
 // 'open' = when connection to PeerServer is established
 peer1.on('open', (id) => console.log(`${id} connected to PeerServer`))
 
 // 'connection'= when connection from a remote Peer is established
-peer1.on('connection', data => console.log(data))
+peer1.on('connection', (dataConnection) => {
+    console.log('[log]: ' + dataConnection.peer + " connected")
+    dataConnection.on('close', () => console.log(`%c[connection]: connection closed for ${dataConnection.peer}`, 'color:red;'))
+    dataConnection.on('data', data => console.log(`[message from ${dataConnection.peer}]: ${data}`))
+
+    dataConnection.on('open', () => {
+        console.log(`%c[connection]: connection ready for ${dataConnection.peer}`, 'color:#35ce35;')
+
+        //DataConnection (receiving messages)
+        const conn = peer1.connect('Zach')
+        conn.on('open', () => {
+            console.log(`%c[connection]: connection with ${conn.peer} established`, 'color:#35ce35;')
+        })
+        conn.on('close', () => {
+            console.log(`%c[connection]: connection with ${conn.peer} lost`, 'color:red;')
+        })
+
+        conn.on('data', data => console.log(`[message from ${conn.peer}]: ${data}`))
+
+    })
+
+    sendMsgBtn.addEventListener('click', () => {
+        dataConnection.send("ITS ME PHILIPP")
+    })
+})
 
 peer1.on("error", (error) => {
     console.error(error)
 })
 
-const conn = peer1.connect('Zach')
-
-conn.on('open', () => {
-    conn.on('data', (data) => console.log('Received data: ' + data))
-    conn.on('error', (err) => console.error(err))
-    conn.send('Hello Zach, I am Philipp.')
-})
-const sendMsgBtn = document.querySelector('.btn')
-sendMsgBtn.addEventListener('click', () => {
-    conn.send("ITS ME PHILIPP")
-    conn.on('error', (err) => console.error(err))
-})
 
 //Video Streams
 const videoStreamBtn = document.querySelector('.video-stream-btn')
